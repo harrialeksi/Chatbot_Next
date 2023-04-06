0c2d4a4841da57feb316f062ecf82aef8dffe748
ShinyHawk <hawkpeter0324@gmail.com>
2023-04-06 23:01:01 +0800

    init

---
+++ app/constant.ts
@@ -0,0 +1,7 @@
+export const OWNER = "mixmaster429";
+export const REPO = "ChatGPT-Next-bot";
+export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
+export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const UPDATE_URL = `${REPO_URL}#keep-updated`;
+export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
+export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
